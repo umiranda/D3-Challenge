62230554cd611590163ff7ef80376695a9eac89f
umiranda <71298635+umiranda@users.noreply.github.com>
2021-01-25 20:52:22 -0500

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height",svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);    
+
+d3.csv("/assets/data.csv").then(function(wholeData){
+    console.log(wholeData);
+}).catch(error => console.log(error))
